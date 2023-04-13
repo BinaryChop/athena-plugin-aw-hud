3a47074d928292cb6fc8c157cbebcc897e3b07d6
Nick <101369051+BinaryChop@users.noreply.github.com>
2023-04-13 19:53:03 +0200

    Versionbump

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # athena-plugin-aw-hud
 HUD v5
+
+Simple HUD for alt:V Athena
+
+F2 switches between HUDs
+
+prepared for SaltyChat & Mobile-Phone (Mobile Phone is not included) (Mobile Phone release soon / Valvetime )
--- shared/config.ts
+++ shared/config.ts
@@ -1,7 +1,7 @@
 export const INDEX_CONFIG = {
     PLUGIN_NAME: 'AW-HUD',
-    VERSION: '1.0.1',
-    ATHENA_VERSION: '5.0.0',
+    VERSION: '1.0.2',
+    ATHENA_VERSION: '5.1.0',
     AUTHORS: ['Styuk', 'BinaryChop'],
 };
 
